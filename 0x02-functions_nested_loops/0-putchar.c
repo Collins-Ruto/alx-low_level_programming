bf299111820894b747ad671e62c947f5b20b405d
collins-ruto <collinsruto48@gmail.com>
2023-07-20 01:40:01 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Main function Start
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Main function Starting point
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
